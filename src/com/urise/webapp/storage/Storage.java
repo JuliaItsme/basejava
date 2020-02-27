e472dce97b57eae5902bc88fe78402a91c52fba7
Julia Goryacheva <juligo04@gmail.com>
2020-02-27 23:25:09 +0300

    lesson06. correction

--- src/com/urise/webapp/storage/Storage.java
+++ src/com/urise/webapp/storage/Storage.java
@@ -2,12 +2,7 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
 import java.util.List;
-
-/**
- * Array based storage for Resumes
- */
 
 public interface Storage {
 
