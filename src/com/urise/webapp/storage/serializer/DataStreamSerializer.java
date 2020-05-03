06da98ad5228801a413e2ee3a089461759b4cd66
Julia Goryacheva <juligo04@gmail.com>
2020-05-03 15:44:45 +0300

    Lesson 10. DataStreamSerializer

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.storage.serializer;
 
 import com.urise.webapp.model.*;
+
 import java.io.*;
 import java.time.LocalDate;
 import java.time.Month;
@@ -15,20 +16,20 @@ public class DataStreamSerializer implements StreamSerializer {
             dos.writeUTF(resume.getFullName());
 
             Map<ContactType, String> contacts = resume.getContacts();
-            writeWithException(dos, contacts.entrySet(), (entry) -> {
+            writeCollection(dos, contacts.entrySet(), entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             });
 
             Map<SectionType, Section> sections = resume.getSections();
-            writeWithException(dos, sections.entrySet(), (entry) -> {
+            writeCollection(dos, sections.entrySet(), (entry) -> {
                 dos.writeUTF(entry.getKey().name());
-                sectionWrite(dos, entry.getKey(), entry.getValue());
+                writeSections(dos, entry.getKey(), entry.getValue());
             });
         }
     }
 
-    private void sectionWrite(DataOutputStream dos, SectionType sectionType, Section section) throws IOException {
+    private void writeSections(DataOutputStream dos, SectionType sectionType, Section section) throws IOException {
         switch (sectionType) {
             case PERSONAL:
             case OBJECTIVE:
@@ -36,16 +37,16 @@ public class DataStreamSerializer implements StreamSerializer {
                 break;
             case ACHIEVEMENT:
             case QUALIFICATIONS:
-                writeWithException(dos, ((ListSection) section).getItems(), dos::writeUTF);
+                writeCollection(dos, ((ListSection) section).getItems(), dos::writeUTF);
                 break;
             case EXPERIENCE:
             case EDUCATION:
-                writeWithException(dos, ((OrganizationSection) section).getOrganizations(), organization -> {
+                writeCollection(dos, ((OrganizationSection) section).getOrganizations(), organization -> {
                     dos.writeUTF(organization.getHomePage().getName());
                     writeCheckNull(dos, organization.getHomePage().getUrl());
-                    writeWithException(dos, organization.getPositions(), position -> {
-                        dateWrite(dos, position.getStartDate());
-                        dateWrite(dos, position.getEndDate());
+                    writeCollection(dos, organization.getPositions(), position -> {
+                        writeLocalDate(dos, position.getStartDate());
+                        writeLocalDate(dos, position.getEndDate());
                         dos.writeUTF(position.getTitle());
                         writeCheckNull(dos, position.getDescription());
                     });
@@ -54,23 +55,23 @@ public class DataStreamSerializer implements StreamSerializer {
         }
     }
 
-    private <KV> void writeWithException(DataOutputStream dos, Collection<KV> collection, ItemWrite<KV> item) throws IOException {
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, WriteElement<T> writer) throws IOException {
         dos.writeInt(collection.size());
-        for (KV kv : collection) {
-            item.itemWrite(kv);
+        for (T item : collection) {
+            writer.write(item);
         }
     }
 
     @FunctionalInterface
-    private interface ItemWrite<KV> {
-        void itemWrite(KV kv) throws IOException;
+    private interface WriteElement<T> {
+        void write(T t) throws IOException;
     }
 
     private void writeCheckNull(DataOutputStream dos, String s) throws IOException {
         dos.writeUTF(s == null ? "" : s);
     }
 
-    private void dateWrite(DataOutputStream dos, LocalDate date) throws IOException {
+    private void writeLocalDate(DataOutputStream dos, LocalDate date) throws IOException {
         dos.writeInt(date.getYear());
         dos.writeInt(date.getMonthValue());
     }
@@ -81,62 +82,63 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
-            readWithException(dis, () -> {
+            readCollection(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readCollection(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
-                resume.addSection(sectionType, sectionRead(dis, sectionType));
+                resume.addSection(sectionType, readSections(dis, sectionType));
             });
             return resume;
         }
     }
 
-    private Section sectionRead(DataInputStream dis, SectionType sectionType) throws IOException {
+    private Section readSections(DataInputStream dis, SectionType sectionType) throws IOException {
         switch (sectionType) {
             case PERSONAL:
             case OBJECTIVE:
                 return new TextSection(dis.readUTF());
             case ACHIEVEMENT:
             case QUALIFICATIONS:
-                return new ListSection(readWithException(dis, (ItemRead<String>) dis::readUTF));
+                return new ListSection(readList(dis, dis::readUTF));
             case EXPERIENCE:
             case EDUCATION:
-                return new OrganizationSection(readWithException(dis, () -> new Organization(
+                return new OrganizationSection(readList(dis, () -> new Organization(
                         new Link(
                                 dis.readUTF(),
                                 readCheckNull(dis)),
-                        readWithException(dis, () -> new Organization.Position(
-                                dateRead(dis),
-                                dateRead(dis),
+                        readList(dis, () -> new Organization.Position(
+                                readLocalDate(dis),
+                                readLocalDate(dis),
                                 dis.readUTF(),
                                 readCheckNull(dis))))));
+            default:
+                throw new IllegalStateException();
         }
-        return null;
     }
 
-    private void readWithException(DataInputStream dis, ItemReads item) throws IOException {
+    private void readCollection(DataInputStream dis, ReadElement reader) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
-            item.itemReads();
+            reader.read();
         }
     }
 
     @FunctionalInterface
-    private interface ItemReads {
-        void itemReads() throws IOException;
+    private interface ReadElement {
+        void read() throws IOException;
     }
 
-    private <KV> List<KV> readWithException(DataInputStream dis, ItemRead<KV> item) throws IOException {
+    private <T> List<T> readList(DataInputStream dis, ReadItem<T> reader) throws IOException {
         int size = dis.readInt();
-        List<KV> list = new ArrayList<>(size);
+        List<T> list = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            list.add(item.itemRead());
+            list.add(reader.read());
         }
         return list;
     }
 
     @FunctionalInterface
-    private interface ItemRead<KV> {
-        KV itemRead() throws IOException;
+    private interface ReadItem<T> {
+        T read() throws IOException;
     }
 
     private String readCheckNull(DataInputStream dis) throws IOException {
@@ -144,7 +146,7 @@ public class DataStreamSerializer implements StreamSerializer {
         return str.equals("") ? null : str;
     }
 
-    private LocalDate dateRead(DataInputStream dis) throws IOException {
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
         return LocalDate.of(dis.readInt(), Month.of(dis.readInt()), 1);
     }
 }
