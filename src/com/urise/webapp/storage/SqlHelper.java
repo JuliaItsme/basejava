20f88813bb1515747bb3f1d394a97b734c6af4a8
Julia Goryacheva <juligo04@gmail.com>
2020-05-12 00:37:31 +0300

    Lesson 13. correction

--- src/com/urise/webapp/storage/SqlHelper.java
+++ src/com/urise/webapp/storage/SqlHelper.java
@@ -5,7 +5,6 @@ import com.urise.webapp.sql.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SqlHelper {
