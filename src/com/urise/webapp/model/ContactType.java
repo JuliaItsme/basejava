cf9e51917f3e93d2fba537b658cccf77acedbab5
Julia Goryacheva <juligo04@gmail.com>
2020-05-28 02:54:15 +0300

    Lesson 16. jspt

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -3,12 +3,42 @@ package com.urise.webapp.model;
 
 public enum ContactType {
     PHONE_NUMBER("Телефонный номер"),
-    SKYPE("Skype"),
-    EMAIL("Почта"),
-    LINKEDIN("LinkedIn"),
-    GITHUB("GitHub"),
-    STACKOVERFLOW("Stackoverflow"),
-    HOME_PAGE("Домашняя страница");
+    SKYPE("Skype") {
+        @Override
+        public String toHtml(String value) {
+            return getTitle() + ": " + toLink("skype:" + value, value);
+        }
+    },
+    EMAIL("Почта") {
+        @Override
+        public String toHtml(String value) {
+            return getTitle() + ": " + toLink("mailto:" + value, value);
+        }
+    },
+    LINKEDIN("LinkedIn") {
+        @Override
+        public String toHtml(String value) {
+            return toLink(value);
+        }
+    },
+    GITHUB("GitHub") {
+        @Override
+        public String toHtml(String value) {
+            return toLink(value);
+        }
+    },
+    STACKOVERFLOW("Stackoverflow"){
+        @Override
+        public String toHtml(String value) {
+            return toLink(value);
+        }
+    },
+    HOME_PAGE("Домашняя страница"){
+        @Override
+        public String toHtml(String value) {
+            return toLink(value);
+        }
+    };
 
     private String title;
 
@@ -18,5 +48,21 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value){
+        return (value == null) ? "" : toHtml0(value);
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
     }
 }
