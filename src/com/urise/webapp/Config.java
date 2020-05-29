1181f6d88dcca3e58abd6848cbb42fd19894b927
Julia Goryacheva <juligo04@gmail.com>
2020-05-29 23:59:46 +0300

    Lesson 17.

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -4,13 +4,12 @@ import com.urise.webapp.storage.SqlStorage;
 import com.urise.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), ".\\config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final Properties props = new Properties();
@@ -18,12 +17,12 @@ public class Config {
     private final Storage storage;
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
@@ -37,14 +36,5 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
-    }
-
-    private static File getHomeDir() {
-        String property = System.getProperty("homeDir");
-        File homeDir = new File(property == null ? "." : property);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
     }
 }
